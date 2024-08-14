687c32f1b12dfa4fc9061a7a964be7bd18275b60
João Ricardo Côre Dutra <jrdutra.com.br@gmail.com>
2024-08-14 00:26:11 -0300

    Inclusão do projeto

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let globalVariable = 'valor inicial';
+
+// Endpoint de healthcheck
+app.get('/healthcheck', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    timestamp: new Date().toISOString()
+  });
+});
+
+io.on('connection', (socket) => {
+  console.log('Um cliente se conectou');
+
+  // Enviar a variável global para o cliente ao conectar
+  socket.emit('update', globalVariable);
+
+  // Receber atualização da variável global do cliente
+  socket.on('updateGlobalVariable', (newValue) => {
+    globalVariable = newValue;
+    io.emit('update', globalVariable); // Atualiza todos os clientes conectados
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Um cliente se desconectou');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Servidor está rodando na porta 3000');
+});
