3bd62083df8f02f524dffcec4f16a9f2e1814994
João Ricardo Côre Dutra <jrdutra.com.br@gmail.com>
2024-08-22 14:52:59 -0300

    Adição do objeto sessao

--- models/Sessao.js
+++ models/Sessao.js
@@ -1,6 +1,9 @@
 class Sessao {
+
     constructor(canal) {
         this.canal = canal;
+        this.updateClientCanal = `updateClient${canal}`;
+        this.updateServerCanal = `updateServer${canal}`;
         this.dataHoraCriacao = new Date();
         this.conteudoTexto = '';
     }
@@ -8,5 +11,6 @@ class Sessao {
     mostrarDetalhes() {
         console.log(`Canal: ${this.canal}`);
         console.log(`Data e Hora de Criação: ${this.dataHoraCriacao}`);
+        return `Criado o canal ${this.canal} em ${this.dataHoraCriacao}`
     }
 }
--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.use((req, res, next) => {
   next();
 });
 
+//const io = socketIo(server);
+const PORT = process.env.PORT || 3000;
+
 let textoGlobal = '';
 
 io.on('connection', (socket) => {
@@ -40,9 +43,6 @@ io.on('connection', (socket) => {
     console.log('Um cliente se desconectou');
   });
 });
-
-//const io = socketIo(server);
-const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log('Servidor está rodando na porta', PORT);
